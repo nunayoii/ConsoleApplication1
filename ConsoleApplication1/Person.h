e064a0ab17bd096344c52120ff34b2f63b115744
nunayoii <4b3g0090@office.stust.edu.tw>
2024-12-22 16:01:09 +0800

    .

---
+++ ConsoleApplication1/Person.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Person
+{
+private:
+	string id;
+	string lastName;
+	string firstName;
+	string gender;
+	string birthDate;
+
+public:
+	
+	Person(const string& id, const string& lastName, const string& firstName, const string& gender, const string& birthDate);
+	Person();
+
+	string getId() const;
+	string getLastName() const;
+	string getFirstName() const;
+	string getGender() const;
+	string getBirthDate() const;
+
+	void setId(const string& id);
+	void setLastName(const string& lastName);
+	void setFirstName(const string& firstName);
+	void setGender(const string& gender);
+	void setBirthDate(const string& birthDate);
+
+	void display() const;
+};
