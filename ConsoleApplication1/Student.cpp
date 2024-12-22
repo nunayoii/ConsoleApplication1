91e105622126ccd30e70fdb0a69c8e683e5c32ab
nunayoii <4b3g0090@office.stust.edu.tw>
2024-12-22 16:21:17 +0800

    .

--- ConsoleApplication1/Student.cpp
+++ ConsoleApplication1/Student.cpp
@@ -39,7 +39,7 @@ void Student::setClassName(ClassName className)
 void Student::display() const
 {
 	Person::display();
-	cout << "????: " << studentId << endl;
-	cout << "???t: " << Utility::toString(department) << endl;
-	cout << "¯Z¯?: " << Utility::toString(className) << endl;
+	cout << "學號: " << studentId << endl;
+	cout << "科系: " << Utility::toString(department) << endl;
+	cout << "班級: " << Utility::toString(className) << endl;
 }
